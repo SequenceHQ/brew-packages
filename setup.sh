ad8482eeee7475b51d7d6ff0ef9a4a78a3064b2e
garry <garry.wilson@sequencehq.com>
2022-02-09 19:15:49 +0000

    Clean up duplicate path lines

--- setup.sh
+++ setup.sh
@@ -68,3 +68,7 @@ sudo -u $mostCommonUser $brew_path install terraform terragrunt
 echo "Installing Kubernetes packages..."
 sudo -u $mostCommonUser $brew_path install kubectl kubectx helm stern
 
+if [[ -f "/Users/${mostCommonUser}/.zshrc" ]]; then
+  cat /Users/${mostCommonUser}/.zshrc | uniq > /Users/${mostCommonUser}/.zshrc.clean && mv /Users/${mostCommonUser}/.zshrc.clean /Users/${mostCommonUser}/.zshrc
+  echo "Trimmed ~/.zshrc"
+fi

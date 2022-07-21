2988a3ea7118401d6191aed24ec26e69ef551a18
garry <garry.wilson@sequencehq.com>
2022-07-21 11:56:45 +0100

    Add the Teleport brew package

--- setup.sh
+++ setup.sh
@@ -60,7 +60,7 @@ sudo -u $mostCommonUser $brew_path install --cask google-cloud-sdk
 
 
 echo "Installing engineering packages..."
-sudo -u $mostCommonUser $brew_path install coreutils jq yamllint adr-tools pre-commit watch c6o/tools/czctl pgcli
+sudo -u $mostCommonUser $brew_path install coreutils jq yamllint adr-tools pre-commit watch c6o/tools/czctl pgcli teleport
 
 echo "Installing SRE packages..."
 sudo -u $mostCommonUser $brew_path install terraform terragrunt

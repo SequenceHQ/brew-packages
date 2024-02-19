42d8cb88a6d588b6f70d36155b355a7d00774898
Adam Strawson <adam@adamstrawson.com>
2024-02-19 11:46:56 +0000

    Add cloudflared to engineer packages (#19)

--- setup.sh
+++ setup.sh
@@ -59,7 +59,7 @@ sudo -u $mostCommonUser $brew_path tap homebrew/core
 sudo -u $mostCommonUser $brew_path install --cask google-cloud-sdk
 
 echo "Installing engineering packages..."
-sudo -u $mostCommonUser $brew_path install coreutils gawk httpie jq pgcli postgresql pre-commit teleport watch yamllint yq
+sudo -u $mostCommonUser $brew_path install cloudflared coreutils gawk httpie jq pgcli postgresql pre-commit teleport watch yamllint yq 
 
 echo "Installing SRE packages..."
 sudo -u $mostCommonUser $brew_path tap liamg/tfsec
